de3181d70de58b0c6d7c0ce6fb4a8b16d08103a6
Eunji Jung <bian87@dgu.ac.kr>
2023-08-13 21:13:33 +0900

    [FEAT] #3 - State, Create

--- lifecoding/src/App.js
+++ lifecoding/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 function Article(props){
   return  <article>
@@ -26,7 +27,7 @@ for(let i=0; i<props.topics.length; i++){
   lis.push(<li key={t.id}>
     <a id={t.id} href = {'/read/'+t.id} onClick={(event)=>{
       event.preventDefault();
-      props.onChangeMode(event.target.id);
+      props.onChangeMode(Number(event.target.id));
     }}>{t.title}</a>
     </li>)
 }
@@ -37,24 +38,73 @@ for(let i=0; i<props.topics.length; i++){
 </nav>
 }
 
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type= "text" name= "title" placeholder="title"/></p>
+      <p><textarea name= "body" placeholder= "body" /></p>
+      <p><input type= "submit" value= "Create" /></p>
+      </form>
+  </article>
+}
+
 
 function App() {
-  const topics = [
-    {id:1, title:'html', boy: 'html is ...'},
-    {id:2, title:'css', boy: 'css is ...'},
-    {id:3, title:'javascript', boy: 'css is ...'},
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    {id:1, title:'html', body: 'html is ...'},
+    {id:2, title:'css', body: 'css is ...'},
+    {id:3, title:'javascript', body: 'css is ...'},
+  ])
 
-  ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  }
+  else if(mode === 'READ'){
+    let title, body = null;
+    for(let i = 0; i < topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+  else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  }
 
   return (
    <div>
     <Header title="WEB" onChangeMode={function(){
-      alert('Header');
+      setMode('WELCOME');
     }}></Header>
-    <Nav topics= {topics} onChangeMode={function(id){
-      alert(id);
+    <Nav topics= {topics} onChangeMode={function(_id){
+      setMode('READ');
+      setId(_id);
     }}></Nav>
-    <Article title="Welcome" body="Hello, Web"></Article>
+    {content}
+    <a href="/create" onClick={(event)=>{
+      event.preventDefault();
+      setMode('CREATE');
+  }}>Create</a>
    </div>
   );
 }
